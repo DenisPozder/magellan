ee5ccfca8d0d2658b9691c72068fd057dd58769b
Denis Pozder <denispozder6@gmail.comm>
2024-01-04 17:31:57 +0100

    Saving Changes

--- src/app/reservations/page.jsx
+++ src/app/reservations/page.jsx
@@ -1,5 +1,6 @@
 import ReservationHero from "./components/ReservationHero/ReservationHero"
 import ReservationSlider from "./components/ReservationSlider/ReservationSlider"
+import styles from './reservations.module.css'
 
 
 const page = () => {
@@ -8,6 +9,9 @@ const page = () => {
     <div>
       <ReservationHero />
       <ReservationSlider />
+      <div className={styles.reservations_paper}>
+        <img src="/paper4.png" alt="Dekorativni papir 4" />
+      </div>
     </div>
     </>
   )

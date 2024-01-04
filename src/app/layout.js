84693e6a71be1a8c904833aa60690336e4a45719
Denis Pozder <denispozder6@gmail.comm>
2024-01-04 16:09:45 +0100

    Saving Changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,9 @@
-import { Inter } from 'next/font/google'
+import { Montserrat } from 'next/font/google'
 import './globals.css'
 import Header from '@/components/layout/Header'
 import Footer from '@/components/layout/Footer'
 
-const inter = Inter({ subsets: ['latin'] })
+const montserrat = Montserrat({ subsets: ['latin'], weight: ['400', '700', '900'] })
 
 export const metadata = {
   title: 'Magellan',
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={montserrat.className}>
           <Header />
             {children}
           <Footer />
